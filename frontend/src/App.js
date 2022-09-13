0f45b5b86ec61c1d7d2f503d6f2c46aacd64e88d
Vashisth Zatakia <zatakiavashisth151@gmail.com>
2022-09-14 01:55:43 +0530

    Routing Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { Routes, Route, BrowserRouter} from "react-router-dom";
+import { Routes, Route, HashRouter} from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -8,14 +8,14 @@ import News from "./components/News";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
       <NavBar/>
         <Routes>
           <Route path="/Quick-crop" element={<Home />}></Route>
           <Route path="/Quick-crop/About" element={<About />}></Route>
           <Route path="/Quick-crop/News" element={<News />}></Route>
         </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </>
   );
 }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/Quick-crop">
           Navbar
         </Link>
         <button
