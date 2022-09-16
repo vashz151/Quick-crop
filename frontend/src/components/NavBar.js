78e6409c51bee1a0309a41e18533b8b35623ef20
Vashisth Zatakia <zatakiavashisth151@gmail.com>
2022-09-17 00:13:22 +0530

    Navbar Updated

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/Quick-crop">
           Quick Crop
