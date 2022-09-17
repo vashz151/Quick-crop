b4b165ed9eb87515bc69ec9890b425a9dabf878c
Vashisth Zatakia <zatakiavashisth151@gmail.com>
2022-09-17 14:54:59 +0530

    News updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,22 @@
 import "./App.css";
 import React from "react";
-import { Routes, Route, HashRouter} from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Home from "./components/Home";
-import About from "./components/About";
+// import { Routes, Route, HashRouter} from "react-router-dom";
+// import NavBar from "./components/NavBar";
+// import Home from "./components/Home";
+// import About from "./components/About";
 import News from "./components/News";
 function App() {
   return (
     <>
-      <HashRouter>
+      {/* <HashRouter>
       <NavBar/>
         <Routes>
           <Route path="/Quick-crop" element={<Home />}></Route>
           <Route path="/Quick-crop/About" element={<About />}></Route>
           <Route path="/Quick-crop/News" element={<News/>}></Route>
         </Routes>
-        </HashRouter>
+        </HashRouter> */}
+        <News/>
     </>
   );
 }
