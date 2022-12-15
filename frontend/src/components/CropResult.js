77ad1d93525c4ecab02b3bdf0a50e20b01b7e4e6
Vashisth Zatakia <zatakiavashisth151@gmail.com>
2022-12-16 01:13:11 +0530

    Added navigation in Crop Result

--- frontend/src/components/CropResult.js
+++ frontend/src/components/CropResult.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../css/cr.css";
 import axios from "axios";
 function CropResult(props) {
   const navigate = useNavigate();
-  const location = useLocation();
   const [results, setResults] = React.useState([]);
   //fetch the data from unsplash api using axios
   React.useEffect(() => {
     axios
       .get("https://api.unsplash.com/search/photos", {
-        params: { query: location.state },
+        params: { query: props.prediction },
         headers: {
           Authorization: `Client-ID ${props.apikey}`,
         },
@@ -18,22 +17,23 @@ function CropResult(props) {
       .then((response) => {
         setResults(response.data.results);
       });
-  }, [location.state, props.apikey]);
+  }, [props.prediction, props.apikey]);
 
   return (
     <div className="crdiv">
       {results
-        .filter((item, index) => index > 1 && index < 3)
+        .filter((item, index) => index === 2)
         .map((item, index) => {
           return (
             <img key={index} id="img" src={item.urls.regular} alt="crop" />
           );
         })}
       <h1 className="text-center" style={{ color: "white" }}>
-        {location.state === "No crop" ? (
-          <p className="para">
+        {props.prediction === "No crop" ? (
+          <div className="para">
             <b>
-              <i>{location.state} </i>can be grown in your farm!.
+              <i style={{ color: "red" }}>{props.prediction} </i>can be grown in
+              your farm!.
             </b>
             <p className="para">
               Try our Fertilizer Recommendation System to increase nutrient
@@ -44,14 +44,16 @@ function CropResult(props) {
               className="crbtn"
               onClick={() => navigate("/fertilizer-recommend")}
             >
-              Fertilizer Recommendation
+              Fertilizer
             </button>
-          </p>
+          </div>
         ) : (
           <p className="para">
             <br />
             <i>
-              You should grow <i>{location.state} </i>in your farm!
+              You should grow{" "}
+              <i style={{ color: "orange" }}>{props.prediction} </i>in your
+              farm!
             </i>
             <br />
             Want to know about the yield of the crop?
