4b8c4c56bcb037a1be2851fb46df82b824eb6fa9
Vashisth <zatakiavashisth151@gmail.com>
2023-05-14 23:03:47 +0530

    Optimised Code

--- backend/api.py
+++
@@ -1,4 +0,0 @@
-weather_api_key = "1dcfacd17d579cb6e46e4c7c4abba849"
-# newsdata_api_key = "pub_1131893c21e8997a0c62bfe990aba48d4f59f"
-# newsdata_api_key = "pub_11366f387421e2f181d6a0b3458d824e857bf"
-newsdata_api_key = "pub_1134612a987b2f1837244594843581dc3a495"
